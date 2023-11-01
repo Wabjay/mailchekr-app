7405f514f5c87d23316b9ec224e282b8b5622a0d
Abdulwahab O. Jimoh <omoniyi.success@gmail.com>
2023-11-01 08:24:47 -0500

    Google Auth

--- src/component/auth/GoogleLogin copy.jsx
+++ src/component/auth/GoogleLogin copy.jsx
@@ -1,52 +1,52 @@
 import Google from "../../assets/images/google.svg";
 import { useGoogleLogin } from "@react-oauth/google";
 
-import axios from "../../helper/api/axios";
-import { useNavigate } from "react-router-dom";
+// import axios from "../../helper/api/axios";
+// import { useNavigate } from "react-router-dom";
 
 const GoogleLogin = () => {
 
 
 
-  const navigate = useNavigate()
+  // const navigate = useNavigate()
 
   const signIn = useGoogleLogin(
-   { 
-   // onSuccess:tokenResponse => console.log(tokenResponse),
-   onSuccess: async tokenResponse => {
-     console.log(tokenResponse);
-     // fetching userinfo can be done on the client or the server
-     const userInfo = await axios
-       .get('https://www.googleapis.com/oauth2/v3/userinfo', {
-         headers: { Authorization: `Bearer ${tokenResponse.access_token}` },
-       })
-       .then(res => res.data);
+//    { 
+//    // onSuccess:tokenResponse => console.log(tokenResponse),
+//    onSuccess: async tokenResponse => {
+//      console.log(tokenResponse);
+//      // fetching userinfo can be done on the client or the server
+//      const userInfo = await axios
+//        .get('https://www.googleapis.com/oauth2/v3/userinfo', {
+//          headers: { Authorization: `Bearer ${tokenResponse.access_token}` },
+//        })
+//        .then(res => res.data);
 
-     console.log(userInfo);
-     await axios.post(
-       "userAuth/sign-in",
-       { email: userInfo.email },
-       {
-         headers: { "Content-Type": "application/json" },
-       }
-     );
-       // setSuccess(success);
-   sessionStorage.setItem('email', userInfo.email)
-   navigate(`/verify/${ userInfo.email }`)
+//      console.log(userInfo);
+//      await axios.post(
+//        "userAuth/sign-in",
+//        { email: userInfo.email },
+//        {
+//          headers: { "Content-Type": "application/json" },
+//        }
+//      );
+//        // setSuccess(success);
+//    sessionStorage.setItem('email', userInfo.email)
+//    navigate(`/verify/${ userInfo.email }`)
 
-   },
-   // flow: 'redirect',
-   // signInFlow: "redirect",
-   onError: () => console.log('Login Failed'),
- },
- // console.log("first")
+//    },
+//    // flow: 'redirect',
+//    // signInFlow: "redirect",
+//    onError: () => console.log('Login Failed'),
+//  },
+ console.log("first")
  );
 
 
   return (
     <button
       className="w-full h-12 flex justify-center items-center text-center text-grey-900 text-[16px] font-medium leading-[22px] rounded-[8px] border-[1px] border-yellow-400 bg-yellow-400"
-      // onClick={signIn}
+      onClick={signIn}
     >
       <img src={Google} className="h-[18px] w-[18px] mr-1" />
       Continue with Google
